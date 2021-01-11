351533a30b28eb7849ff5a40d334dd9961cc3090
Luke Oliver <galactic.fungus@gmail.com>
2021-01-11 20:22:12 +1100

    API for sub byte data structures Packed data structure read in one go, ie as u16

--- src/dns.rs
+++ src/dns.rs
@@ -5,17 +5,26 @@ use std::{fmt::DebugStruct, io::BufReader, io::Write};
 pub struct DnsPacket {
     header: Header,
 }
-#[derive(Debug)]
-pub enum QueryResponse {
+#[derive(Debug, Copy, Clone)]
+pub enum PacketType {
     Query = 0,
     Response = 1,
 }
 
-impl From<u8> for QueryResponse {
+impl From<PacketType> for u16 {
+    fn from(packet_type: PacketType) -> Self {
+        match packet_type {
+            PacketType::Query => 0,
+            PacketType::Response => 1,
+        }
+    }
+}
+
+impl From<u8> for PacketType {
     fn from(value: u8) -> Self {
         match value {
-            0 => QueryResponse::Query,
-            1 => QueryResponse::Response,
+            0 => PacketType::Query,
+            1 => PacketType::Response,
             // This whole thing should be a tryfrom and return an error
             _ => unreachable!("A query response that was neither 0 or 1 was found"),
         }
@@ -40,7 +49,7 @@ impl From<u8> for OperationCode {
     }
 }
 
-impl From<OperationCode> for u8 {
+impl From<OperationCode> for u16 {
     fn from(value: OperationCode) -> Self {
         match value {
             OperationCode::StandardQuery => 0,
@@ -78,11 +87,28 @@ impl From<u8> for ResponseCode {
     }
 }
 
+impl From<ResponseCode> for u16 {
+    fn from(code: ResponseCode) -> Self {
+        match code {
+            ResponseCode::FORMERR => 1,
+            ResponseCode::SERVFAIL => 2,
+            ResponseCode::NXDOMAIN => 3,
+            ResponseCode::NOTIMP => 4,
+            ResponseCode::REFUSED => 5,
+            ResponseCode::NOERROR => 0,
+            ResponseCode::UNKNOWN => {
+                unreachable!("An unknown response code can't be used in a DNS Query or response")
+            }
+        }
+    }
+}
+
 pub struct RawPacket {
     data: [u8; 512],
 }
 
 impl RawPacket {
+    // TODO: Both bitmasks can be combined into a single U16 bitmask rather than two seperate bitmasks
     fn write_header(packet_cursor: &mut Cursor<&mut [u8]>, header: &Header) -> Result<(), Error> {
         // ID
         packet_cursor
@@ -90,35 +116,51 @@ impl RawPacket {
             .map_err(|err| Error::new(ErrorKind::WritePacketDataFailed, Some(err)))?;
         // First bitmask consists of
         // QueryResponse 1 bit field
-        let mut first_bitmask = match header.queury_response {
-            QueryResponse::Query => 0,
-            QueryResponse::Response => 1 << 7, // 128 as we are converting from little endian bits to network endian bits
-        };
+        let mut bitmask = 0;
+        RawPacket::set_bit_position(0, 1, &mut bitmask, header.queury_response.into());
         // Opcode 4 bit field
-        let opcode_value: u8 = header.operation_code.into();
-        first_bitmask |= opcode_value << 3;
+        RawPacket::set_bit_position(1, 4, &mut bitmask, header.operation_code.into());
         // Authorative 1 bit field
-        first_bitmask |= match header.authorative {true => 1, false => 0} << 2;
+        RawPacket::set_bit_position(5, 1, &mut bitmask, header.authorative.into());
         // Truncation 1 bit field
-        first_bitmask |= match header.truncated {true => 1, false => 0 } << 1;
+        RawPacket::set_bit_position(6, 1, &mut bitmask, header.truncated.into());
         // Recursive Desired 1 bit field
-        first_bitmask |= match header.recursion_desired {true => 1, false => 0};
-        packet_cursor.write_u8(first_bitmask)
+        RawPacket::set_bit_position(7, 1, &mut bitmask, header.recursion_desired.into());
+        // Recursion Available 1 bit field
+        RawPacket::set_bit_position(8, 1, &mut bitmask, header.recursion_available.into());
+        // Z 3 bit field - for now we write 0
+        RawPacket::set_bit_position(9, 3, &mut bitmask, 0);
+        // Response Code 4 bit field
+        RawPacket::set_bit_position(12, 4, &mut bitmask, header.response_code.into());
+        packet_cursor
+            .write_u16::<NetworkEndian>(bitmask)
             .map_err(|err| Error::new(ErrorKind::WritePacketDataFailed, Some(err)))?;
-        // Second bit mask consists of
         Ok(())
     }
-    // TODO: Do we translate from right based bit to left based bit
+
+    fn set_bit_position(position: u8, bit_length: u8, data: &mut u16, bits_to_set: u16) {
+        // To set bit position 1 with data that is 4 bits long
+        // 15 - position
+        // 16 - (1 + 4) = 11 meaning we shift left 11 places to place the start of a 4 bit value at position 1
+        // To set bit position 11 with data that is 2 bits long
+        // 16 - (11 + 2) = 13
+        debug_assert!(position < 16);
+        debug_assert!(bit_length < 16);
+        let translated_position = 16 - (position + bit_length);
+        let r = bits_to_set << translated_position;
+        *data |= r;
+    }
+
     /// The bit position is based on the positions in RFC 1035, meaning that bit positon 0 is the left most bit,
-    /// this is the opposite of normal on most systems where the MSB (Most Significant Bit) is the right one
-    fn set_bit_position(position: u8, bit_length: u8, data: &mut u8, bits_to_set: u8) {
-        // Bit position 3, 4 bits long
-        // bit length doesn't matter
-        // or bit position 1
+    /// this is the opposite of normal on most systems where the LSB (Least Significant Bit) is the left most bit
+    fn set_bit_position_u8(position: u8, bit_length: u8, data: &mut u8, bits_to_set: u8) {
+        // To set bit position 1 with data that is 4 bits long
         // 8 - (1 + 4) = 3
-        // position =
-        // 8 - 4 = 4 which is 3 in network bits
-        *data |= bits_to_set << 3;
+        assert!(position < 7);
+        assert!(bit_length < 8);
+        let translated_position = 8 - (bits_to_set + bit_length);
+        assert!(translated_position < 8);
+        *data |= bits_to_set << translated_position;
     }
 }
 
@@ -130,7 +172,7 @@ impl TryFrom<DnsPacket> for RawPacket {
         let mut packet_cursor = std::io::Cursor::new(writeable_slice);
         // let mut packet_writer = std::io::BufWriter::new(&mut writeable_slice);
         RawPacket::write_header(&mut packet_cursor, &dns_packet.header)?;
-        
+
         Ok(raw_packet)
     }
 }
@@ -156,7 +198,7 @@ impl TryFrom<RawPacket> for DnsPacket {
 #[derive(Debug)]
 pub struct Header {
     id: u16,
-    queury_response: QueryResponse,
+    queury_response: PacketType,
     operation_code: OperationCode,
     authorative: bool,
     truncated: bool,
@@ -178,7 +220,7 @@ impl Header {
             recursion_desired: false,
             authorative: false,
             operation_code: OperationCode::StandardQuery,
-            queury_response: QueryResponse::Query,
+            queury_response: PacketType::Query,
             response_code: ResponseCode::NOERROR,
             question_count: 0,
             answer_count: 0,
@@ -186,6 +228,22 @@ impl Header {
             additional_count: 0,
             truncated: false,
         }
+    }
+
+    fn get_bit_position(position: u8, bit_length: u8, source: &u16) -> u8 {
+        // 1 becomes 1
+        // 2 becomes 3
+        // 3 necomes 7
+        // 4 becomes 15
+        // 5 becomes 31
+        // Values greater than 8 are possible, for instance its possible to support a value that takes 14 bits
+        // Before masking we move the required bits into place
+        let translated_position = 16 - (position + bit_length);
+        let result = *source >> translated_position;
+        debug_assert!(bit_length < 8);
+        let length_mask = 2u16.pow(bit_length as u32) - 1;
+        // The high bits of this value should always be masked out and be zero since no individual value will have more than 7 bits
+        (result & length_mask) as u8
     }
 
     // Read the header of a DNS packet, requires a cursor for any type that can be turned into a reference to a u8 slice
@@ -196,27 +254,24 @@ impl Header {
             .map_err(|err| Error::new(ErrorKind::ReadPacketDataFailed, Some(err)))?;
         // The next byte is made up a bitmask
         let bitmask = packet_data
-            .read_u8()
+            .read_u16::<NetworkEndian>()
             .map_err(|err| Error::new(ErrorKind::ReadPacketDataFailed, Some(err)))?;
         // QR 1 bit field
-        let query_response = QueryResponse::from((bitmask >> 7) & 1);
+        let query_response = PacketType::from(Header::get_bit_position(0, 1, &bitmask));
         // Op code 4 bit field
-        let op_code = OperationCode::from((bitmask >> 6) & 15);
+        let op_code = OperationCode::from(Header::get_bit_position(1, 4, &bitmask));
         // Authoritative 1 bit field
-        let ar = (bitmask >> 2) & 1 == 1;
+        let ar: bool = Header::get_bit_position(5, 1, &bitmask) == 1;
         // Truncation 1 bit field
-        let truncation = (bitmask >> 1) & 1 == 1;
+        let truncation = Header::get_bit_position(6, 1, &bitmask) == 1;
         // Recursion Desired 1 bit field
-        let recursion_desired = bitmask & 1 == 1;
-        // Read the next byte
-        let bitmask2 = packet_data
-            .read_u8()
-            .map_err(|err| Error::new(ErrorKind::ReadPacketDataFailed, Some(err)))?;
+        let recursion_desired = Header::get_bit_position(7, 1, &bitmask) == 1;
         // Recursion Available 1 bit field
-        let recursion_available = (bitmask2 >> 7) & 1 == 1;
+        let recursion_available = Header::get_bit_position(8, 1, &bitmask) == 1;
         // Z Ignore for now 3 bit field
+        let z = Header::get_bit_position(9, 3, &bitmask);
         // Response Code 4 bit field
-        let response_code = ResponseCode::from((bitmask2 >> 4) & 15);
+        let response_code = ResponseCode::from(Header::get_bit_position(12, 4, &bitmask));
         // --
         // Question Count 16 bit field
         let question_count = packet_data
@@ -302,5 +357,21 @@ mod tests {
         let header = Header::read_header(&mut packet_reader).unwrap();
         println!("{:?}", header);
         // Response code isn't used for queries
+    }
+
+    #[test]
+    fn test_get_bit_position() {
+        let mut test_value = 0;
+        RawPacket::set_bit_position(0, 2, &mut test_value, 3);
+        // Two highest bits should be set
+        assert_eq!(test_value, 0b1100000000000000u16);
+        let data = Header::get_bit_position(0, 2, &test_value);
+        assert_eq!(data, 3);
+        // 9 from the left not the right
+        RawPacket::set_bit_position(9, 1, &mut test_value, 1);
+        let test_bool = Header::get_bit_position(9, 1, &test_value) == 1;
+        assert!(test_bool);
+        println!("Final test value: {:b} vs expected {:b}", test_value, 0b1100000001000000u16);
+        assert_eq!(test_value, 0b1100000001000000u16);
     }
 }
